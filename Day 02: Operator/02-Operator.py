52bdfe03082f74e282cbd166f97761bf266bb843
shivamnamdev <shivamnamdev.edu@gmail.com>
2026-04-07 21:01:14 +0530

    Day-02: Operator

---
+++ Day 02: Operator/02-Operator.py
@@ -0,0 +1,87 @@
+# # Operator: To perform operation between 2 or more that 2 variable/Constant
+
+# # 1. Arithmetic Operator
+# # 2. Logical Operator
+# # 3. Comparitive Operator
+# # 4. Membership Operator
+# # 5. identity Operator
+
+# # 1. Arithmetic Operator
+
+# # + - * / // ** %
+
+a = 2
+b = 4
+c = 6
+d = 4
+# print(a + b) # 6
+# print(a - b) # -2
+# print(a * b) # 8
+# print(a / b) # 0.5
+# print(a // b) # only decimal 0
+# print(a ** b) # power 16
+# print(a % b) # remainder 2
+print(c//a*b+d-a) # 14
+10
+14
+40
+
+
+# 2. Logical Operator
+# or - and - not
+
+a = True
+b = False
+
+# print(True or True) # True
+# print(False or True) # True
+# print(True or False) # True
+# print(False or False) # False
+
+# print(True and True) # True
+# print(False and True) # False
+# print(True and False) # False
+# print(False and False) # False
+
+# print(not True) # False
+# print(not False) # True
+
+# int
+# False -> 0
+# True -> anything apart from 0
+
+# Float
+# False -> 0.0
+# True -> anything apart from 0.0
+
+# String
+# False -> ""
+# True -> "<anything>"
+
+
+# print("" or "No") # True
+# print("" or 0 or 1 or False) # True False
+# print("Yes" and "No") # True
+# print("Yes" and "No" and "") # False
+# print("1" or 1) # True 
+# print(0 or "") # False
+# print(not -1) # False
+# print(not "") # True
+# print(not "String") #False
+# print(not None) # True
+
+# 3. Comparitive Operator
+# < > <= >= == !=
+
+# a = 2
+# b = 4
+
+# print(a < b) # less than -> True
+# print(a > b) # greater than -> False
+# print(a <= b) # < or = -> True
+# print(a >= b) # > or = -> False
+# print(a != b) # not = -> True
+# print(a == b) # both equal -> False
+
+
+# print(("None" and None) or 0)
