d552d58fa451413726471bb916fad8402322ecf4
shivamnamdev <shivamnamdev.edu@gmail.com>
2026-04-14 09:47:05 +0530

    Day-06: While Loop

---
+++ Day 06: Looping/06-Loops.py
@@ -0,0 +1,51 @@
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+
+# initialization
+# while <condition>:
+#     action 
+
+# y = "" #initialization
+# while y != "no": #while condition
+#     print("Hello")
+#     y = input("Please give in the form of yes and no: ")
+    
+# flag = 1    
+# while flag:
+#     print("working")    
+#     flag = input("0 and 1: ") 
+    
+    
+# flag = False
+# while not flag:
+#     print("working") 
+#     flag = True      
+    
+    #c, c++, java
+    # while(condition) {
+    # action
+    # } 
+# while True:
+#     print()
+    
+# increment = 0
+# while increment < 5:
+#     print("Hello")
+#     increment= increment +2 
+#     increment = increment -1  
+# print("end")    
+# 1 -> inc = 0, print hello, inc=2
+# 2 -> inc = 2, print hello, inc=4
+# 3 -> inc = 4, print hello, inc=6
+# ---
+# 5 --> inc = 5
+
+a = ""
+
+while a != "Loop ended":
+    a = input("Enter your preference:")
+    print("Works with:", a)  
+print("end")      
